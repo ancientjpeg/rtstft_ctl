28768c198bc12d0d08bd96f88e4697095a92d6c8
Jackson Kaplan <JwyattK@gmail.com>
2022-05-03 08:28:47 -0700

    adding PresetChooser

--- Source/Utilities/RT_FileTree.cpp
+++ Source/Utilities/RT_FileTree.cpp
@@ -9,3 +9,12 @@
 */
 
 #include "RT_FileTree.h"
+
+RT_FileTree::RT_FileTree(juce::File inTreeRootFile) {}
+RT_FileTree::~RT_FileTree() {}
+RT_FileTree::RT_FileTree(RT_FileTree &&InFileTreeRvRef) {}
+RT_FileTree &RT_FileTree::operator=(RT_FileTree &&InFileTreeRvRef) {}
+
+juce::File                RT_FileTree::getTreeRootDirObject() {}
+
+void RT_FileTree::constructFileTree(juce::File inTreeRootFile) {}
--- Source/Utilities/RT_FileTree.h
+++ Source/Utilities/RT_FileTree.h
@@ -9,3 +9,22 @@
 */
 
 #pragma once
+#include <JuceHeader.h>
+
+class RT_FileTree {
+public:
+  RT_FileTree(juce::File inTreeRootFile);
+  ~RT_FileTree();
+  RT_FileTree(RT_FileTree &&InFileTreeRvRef);
+  RT_FileTree &operator=(RT_FileTree &&InFileTreeRvRef);
+
+  juce::File   getTreeRootDirObject();
+
+private:
+  void                     constructFileTree(juce::File inTreeRootFile);
+  bool                     mIsRoot;
+  juce::File               mNodeDirFileObject;
+  juce::Array<juce::File>  mFilesInDir;
+  juce::Array<RT_FileTree> mDirsInDir;
+  RT_FileTree             *mParent = nullptr;
+};
