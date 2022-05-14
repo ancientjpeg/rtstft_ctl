7f4b94a4143b573814cc45242aa241720634953d
Jackson Kaplan <JwyattK@gmail.com>
2022-05-14 11:18:19 -0700

    packager works I think??

--- build.sh
+++ build.sh
@@ -1,5 +1,60 @@
 #!/bin/sh
-PKG_DIR="pkg_build"
+
+VERSION="0.1.0"
+PLUGIN_DIR="$HOME/Desktop/test"
+PLUGIN_DIR="/Library/Audio/Plug-Ins"
+
+mkdir pkg_build
+cd pkg_build
+
+cat > distribution.xml << XMLEND
+<?xml version="1.0" encoding="UTF-8"?>
+<installer-gui-script>
+    <options/>
+    <title>rtstft_ctl</title>
+    <background file="background" scaling="tofit" alignment="center"/>
+    <welcome file="readme.txt"  mime-type="text/plain" />
+    <license file="license.txt" mime-type="text/plain" />
+    <choices-outline>
+        <line choice="pick0"/>
+        <line choice="pick1"/>
+    </choices-outline>
+    <choice id="pick0" title="Audio Unit" description="rtstft_ctl Audio Unit Installation">
+        <pkg-ref id="com.soundctl.rtstftctl.au"/>
+    </choice>
+    <choice id="pick1" title="VST3" description="rtstft_ctl VST3 Installation">
+        <pkg-ref id="com.soundctl.rtstftctl.vst3"/>
+    </choice>
+    <pkg-ref id="com.soundctl.rtstftctl.au" version="0.1.0" auth="Root">rtstft_ctl.component.pkg</pkg-ref>
+	<pkg-ref id="com.soundctl.rtstftctl.vst3" version="0.1.0" auth="Root">rtstft_ctl.vst3.pkg</pkg-ref>
+</installer-gui-script>
+XMLEND
+
+
+
+echo $"======== CREATING TXT FILES  ========\n"
+
+echo "Welcome to the rtstft_ctl installer." > readme.txt
+echo "license" > license.txt
+
+echo $"======== CREATING TXT FILES  ========\n"
+
+
+echo $"======= CREATING SUB-PACKAGES =======\n"
+
+AU_NAME="rtstft_ctl.component"
+AU_SRC="../Builds/MacOSX/build/Release/"$AU_NAME
+AU_DEST=$PLUGIN_DIR"/Components"
+AU_ID="com.soundctl.rtstftctl.au"
+
+
+VST3_NAME="rtstft_ctl.vst3"
+VST3_SRC="../Builds/MacOSX/build/Release/"$VST3_NAME
+VST3_DEST=$PLUGIN_DIR"/VST3"
+VST3_ID="com.soundctl.rtstftctl.vst3"
+
+DIST_SIG="Apple Distribution: Jackson Kaplan"
+PKG_SIG="3rd Party Mac Developer Installer: Jackson Kaplan"
 
 asssemble_pkg() {
     SRC="$1"
@@ -11,29 +66,28 @@ asssemble_pkg() {
         echo "Improper parameters in pkg assembly phase; check the build script."
         exit 1 
     fi
+
+    cp -r $SRC $NAME
+    codesign -s "$DIST_SIG" $NAME
   
     pkgbuild \
-    --component "$SRC" \
+    --component "$NAME" \
     --identifier "$ID" \
+    --version $VERSION \
     --install-location "$DEST" \
-    $PKG_DIR"/"$NAME.pkg 
-
+    --sign "$PKG_SIG" \
+    $NAME.pkg # > /dev/null 2>&1
+    echo $"\n"
 }
 
-PLUGIN_DIR="$HOME/Desktop/test"
-
-AU_NAME="rtstft_ctl.component"
-AU_SRC="Builds/MacOSX/build/Release/"$AU_NAME
-AU_DEST=$PLUGIN_DIR"/Components"
-AU_ID="com.soundctl.rtstftctl.au"
-
-
-VST3_NAME="rtstft_ctl.vst3"
-VST3_SRC="Builds/MacOSX/build/Release/"$VST3_NAME
-VST3_DEST=$PLUGIN_DIR"/VST3"
-VST3_ID="com.soundctl.rtstftctl.vst3"
-
-mkdir pkg_build
-asssemble_pkg $VST3_SRC $VST3_DEST $VST3_NAME $VST3_ID
 asssemble_pkg $AU_SRC $AU_DEST $AU_NAME $AU_ID
-# rm -rf pkg_build
+asssemble_pkg $VST3_SRC $VST3_DEST $VST3_NAME $VST3_ID
+
+echo $"======== FINAL PACKAGE BUILD ========\n"
+PKG_NAME_FINAL="OSX_rtstft_ctl_"$VERSION".pkg"
+productbuild --sign "$PKG_SIG" --distribution distribution.xml --package-path ./ $PKG_NAME_FINAL # > /dev/null 2>&1
+
+echo $"\n============== CLEANUP ==============\n"
+mv $PKG_NAME_FINAL ..
+cd ..
+rm -rf pkg_build
