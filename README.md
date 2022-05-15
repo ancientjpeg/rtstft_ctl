a07ba77463151b310df6efc7faed5b020c295212
Jackson Kaplan <JwyattK@gmail.com>
2022-05-15 09:40:16 -0700

    added license

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# rtstft\_ctl
+# rtstft_ctl
 
-A companion plugin to the [RTSTFT](https://github.com/ancientjpeg/RTSTFT) library, built in JUCE. 
+A companion plugin to the [RTSTFT](https://github.com/ancientjpeg/RTSTFT) library, built in JUCE.
 
 # Compilation
 
-
-You will require an installation of [JUCE](https://juce.com/get-juce), including the Projucer, to build this project. Further, you will need to initialize this project's submodules. A full setup of the rtstft\_ctl build folder can be done by executing these commands:
+You will require an installation of [JUCE](https://juce.com/get-juce), including the Projucer, to build this project. Further, you will need to initialize this project's submodules. A full setup of the rtstft_ctl build folder can be done by executing these commands:
 
 ```
 git clone "https://github.com/ancientjpeg/rtstft_ctl"
@@ -16,3 +15,25 @@ git submodule init && git submodule update
 ```
 
 At this point, RTSTFT should be fully set up along with its dependency, PFFFT. You can then open the .jucer file and build the project using your favorite exporter (This has only been tested on macOS, however–installers and Windows verification coming soon!).
+
+## License
+
+See LICENSE for more details.
+
+```
+rtstft_ctl is an audio plug-in based off JUCE and the RTSTFT Library.
+Copyright (C) 2022 Jackson Kaplan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+```
--- build.sh
+++ build.sh
@@ -7,28 +7,8 @@ PLUGIN_DIR="/Library/Audio/Plug-Ins"
 mkdir pkg_build
 cd pkg_build
 
-cat > distribution.xml << XMLEND
-<?xml version="1.0" encoding="UTF-8"?>
-<installer-gui-script>
-    <options/>
-    <title>rtstft_ctl</title>
-    <background file="background" scaling="tofit" alignment="center"/>
-    <welcome file="readme.txt"  mime-type="text/plain" />
-    <license file="license.txt" mime-type="text/plain" />
-    <choices-outline>
-        <line choice="pick0"/>
-        <line choice="pick1"/>
-    </choices-outline>
-    <choice id="pick0" title="Audio Unit" description="rtstft_ctl Audio Unit Installation">
-        <pkg-ref id="com.soundctl.rtstftctl.au"/>
-    </choice>
-    <choice id="pick1" title="VST3" description="rtstft_ctl VST3 Installation">
-        <pkg-ref id="com.soundctl.rtstftctl.vst3"/>
-    </choice>
-    <pkg-ref id="com.soundctl.rtstftctl.au" version="0.1.0" auth="Root">rtstft_ctl.component.pkg</pkg-ref>
-	<pkg-ref id="com.soundctl.rtstftctl.vst3" version="0.1.0" auth="Root">rtstft_ctl.vst3.pkg</pkg-ref>
-</installer-gui-script>
-XMLEND
+# https://github.com/kurasu/surge/blob/main/installer_mac/make_installer.sh
+
 
 
 
@@ -37,10 +17,7 @@ echo $"======== CREATING TXT FILES  ========\n"
 echo "Welcome to the rtstft_ctl installer." > readme.txt
 echo "license" > license.txt
 
-echo $"======== CREATING TXT FILES  ========\n"
-
-
-echo $"======= CREATING SUB-PACKAGES =======\n"
+echo $"===== CREATING DISTRIBUTION XML =====\n"
 
 AU_NAME="rtstft_ctl.component"
 AU_SRC="../Builds/MacOSX/build/Release/"$AU_NAME
@@ -52,6 +29,33 @@ VST3_NAME="rtstft_ctl.vst3"
 VST3_SRC="../Builds/MacOSX/build/Release/"$VST3_NAME
 VST3_DEST=$PLUGIN_DIR"/VST3"
 VST3_ID="com.soundctl.rtstftctl.vst3"
+
+cat > distribution.xml << XMLEND
+<?xml version="1.0" encoding="UTF-8"?>
+<installer-gui-script>
+    <options/>
+    <title>rtstft_ctl</title>
+    <background file="background" scaling="tofit" alignment="center"/>
+    <welcome file="readme.txt"  mime-type="text/plain" />
+    <license file="license.txt" mime-type="text/plain" />
+    <choices-outline>
+        <line choice="${AU_NAME}"/>
+        <line choice="${VST_NAME}"/>
+    </choices-outline>
+    <choice id="${AU_NAME}" title="Audio Unit" description="rtstft_ctl Audio Unit Installation">
+        <pkg-ref id="${AU_ID}"/>
+    </choice>
+    <choice id="${VST_NAME}" title="VST3" description="rtstft_ctl VST3 Installation">
+        <pkg-ref id="${VST_ID}"/>
+    </choice>
+    <pkg-ref id="${AU_ID}" version="0.1.0" auth="Root">${AU_NAME}.pkg</pkg-ref>
+	<pkg-ref id="${VST_ID}" version="0.1.0" auth="Root">${VST_NAME}.pkg</pkg-ref>
+</installer-gui-script>
+XMLEND
+
+echo $"======= CREATING SUB-PACKAGES =======\n"
+
+
 
 DIST_SIG="Apple Distribution: Jackson Kaplan"
 PKG_SIG="3rd Party Mac Developer Installer: Jackson Kaplan"
